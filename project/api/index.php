3b7e2ae4638c1f93d24edf6b46088f2f593c81ae
leonardsmalls <cubecubed333@gmail.com>
2024-04-21 13:02:11 -0500

    init

---
+++ project/api/index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>API</title>
+    </head>
+    <body>
+        <h1>API</h1>
+        <p>API is working</p>
+
+<?php
+phpinfo();
+?>
+
+    </body>
+</html>
+```
+I have tried to access the API using the following URL:
+```
